a3425dfaaed7fa1599257b6564238441a8a8bade
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-15 15:57:35 +0000

    Edit post feature implemented.

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,8 @@ from django.urls    import path
 from .views         import get_posts, post_detail, create_or_edit_post
 
 urlpatterns = [
-    path('<pk>/', post_detail, name='post_detail'),
+    path('<pk>/new', create_or_edit_post, name='new_post'),
     path('<pk>/edit', create_or_edit_post, name='edit_detail'),
+    path('<pk>/', post_detail, name='post_detail'),
     path('', get_posts, name='get_posts'),
 ]
